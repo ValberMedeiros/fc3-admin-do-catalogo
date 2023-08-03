df2c2c0121ef70c34d4b1b7bab24f34cd9193ebe
ValberMedeiros <dev.juniormedeiros@gmail.com>
2023-08-03 20:39:22 -0300

    adicionado UseCase de criação de categoria

--- application/src/main/java/com/fullcycle/admin/catalogo/application/UseCase.java
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/UseCase.java
@@ -1,10 +1,7 @@
 package com.fullcycle.admin.catalogo.application;
 
-import com.fullcycle.admin.catalogo.domain.category.Category;
+public abstract class UseCase<IN, OUT> {
 
-public class UseCase {
+    public abstract OUT execute(IN anIN);
 
-    public Category execute() {
-        return Category.newCategory("teste", "teste", true);
-    }
 }
